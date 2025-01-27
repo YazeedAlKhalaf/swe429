69e777118c187184d8712b32fe18db6b6c66bf9c
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2025-01-27 10:26:44 +0300

    enhance the gcd to use the new style

--- pkg/gcd/gcd.go
+++ pkg/gcd/gcd.go
@@ -1,22 +1,15 @@
 package gcd
 
-import (
-	"fmt"
-)
-
-type GCD struct {
-}
-
-func (g GCD) Euclid(m, n int) int {
+func Euclid(m, n int) int {
 	if n == 0 {
 		return m
 	}
 
 	r := m % n
-	return g.Euclid(n, r)
+	return Euclid(n, r)
 }
 
-func (g GCD) ConsecutiveIntCheck(m, n int) int {
+func ConsecutiveIntCheck(m, n int) int {
 	t := m
 	if n < t {
 		t = n
@@ -33,11 +26,4 @@ func (g GCD) ConsecutiveIntCheck(m, n int) int {
 	}
 
 	return -1
-}
-
-func main() {
-	g := GCD{}
-
-	fmt.Printf("Euclide: %d\n", g.Euclid(60, 24))
-	fmt.Printf("ConsecutiveIntCheck: %d\n", g.ConsecutiveIntCheck(60, 24))
 }
--- pkg/gcd/gcd_test.go
+++ pkg/gcd/gcd_test.go
@@ -1,1 +1,22 @@
 package gcd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// g := GCD{}
+
+// 	fmt.Printf("Euclide: %d\n", g.Euclid(60, 24))
+// 	fmt.Printf("ConsecutiveIntCheck: %d\n", g.ConsecutiveIntCheck(60, 24))
+
+func TestEuclid_60And24(t *testing.T) {
+	res := Euclid(60, 24)
+	assert.Equal(t, 12, res)
+}
+
+func TestConsecutiveIntCheck_60And24(t *testing.T) {
+	res := ConsecutiveIntCheck(60, 24)
+	assert.Equal(t, 12, res)
+}
