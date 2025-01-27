475ac06168f9565c1bcba0c3b0c2bc6486481988
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2025-01-27 10:41:36 +0300

    enhance fibonacci and make it use big.Int

--- pkg/fibonacci/fibonacci.go
+++ pkg/fibonacci/fibonacci.go
@@ -1,26 +1,28 @@
 package fibonacci
 
-func Fib(n int) int {
+import "math/big"
+
+func Fib(n uint64) *big.Int {
 	if n == 0 || n == 1 {
-		return n
+		return big.NewInt(int64(n))
 	}
 
-	return Fib(n-1) + Fib(n-2)
+	return new(big.Int).Add(Fib(n-1), Fib(n-2))
 }
 
-func FibOptimized(n int) int {
-	return fibOptimizedInternal(n, make(map[int]int))
+func FibOptimized(n uint64) *big.Int {
+	return fibOptimizedInternal(n, make(map[uint64]*big.Int))
 }
 
-func fibOptimizedInternal(n int, m map[int]int) int {
+func fibOptimizedInternal(n uint64, m map[uint64]*big.Int) *big.Int {
 	if n == 0 || n == 1 {
-		return n
+		return big.NewInt(int64(n))
 	}
 
 	if val, exists := m[n]; exists {
 		return val
 	}
 
-	m[n] = fibOptimizedInternal(n-1, m) + fibOptimizedInternal(n-2, m)
+	m[n] = new(big.Int).Add(fibOptimizedInternal(n-1, m), fibOptimizedInternal(n-2, m))
 	return m[n]
 }
--- pkg/fibonacci/fibonacci_test.go
+++ pkg/fibonacci/fibonacci_test.go
@@ -2,6 +2,7 @@ package fibonacci
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 	"time"
 
@@ -9,22 +10,40 @@ import (
 )
 
 func TestFib_Gives10thFibonacci(t *testing.T) {
-	n := 10
+	n := uint64(10)
 	res := Fib(n)
 	fmt.Printf("fib of %d: %d\n", n, Fib(n))
 
-	assert.Equal(t, 55, res)
+	assert.Equal(t, big.NewInt(55), res)
 }
 
 func TestFibOptimized_Gives10thFibonacci(t *testing.T) {
-	n := 10
+	n := uint64(10)
 	res := FibOptimized(n)
 	fmt.Printf("fib optmized of %d: %d\n", n, res)
 
-	assert.Equal(t, 55, res)
+	assert.Equal(t, big.NewInt(55), res)
 }
 
-func runComparisonTest(t *testing.T, n int) {
+func TestFibOptimized_Gives500thFibonacci(t *testing.T) {
+	n := uint64(500)
+	res := FibOptimized(n)
+	fmt.Printf("fib optimized of %d: %s\n", n, res.String())
+
+	expected, _ := new(big.Int).SetString("139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125", 10)
+	assert.Equal(t, expected, res)
+}
+
+func TestFibOptimized_Gives1000thFibonacci(t *testing.T) {
+	n := uint64(1000)
+	res := FibOptimized(n)
+	fmt.Printf("fib optimized of %d: %s\n", n, res.String())
+
+	expected, _ := new(big.Int).SetString("43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875", 10)
+	assert.Equal(t, expected, res)
+}
+
+func runComparisonTest(t *testing.T, n uint64) {
 	startOptimized := time.Now()
 	fmt.Printf("fib optmized %d: %d\n", n, FibOptimized(n))
 	endOptimized := time.Now()
